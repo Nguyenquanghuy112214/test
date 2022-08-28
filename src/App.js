fe3106a3845cbd97db840fec3dad17ec605f9401
QuangHuy <nguyenquanghuy112214@gmail>
2022-08-28 21:13:08 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,21 +1,698 @@
 import logo from './logo.svg';
 import './App.css';
-import music1 from './music/music1.mp3'
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Test Github.io
-        </p>
-        <audio controls>
-          <source src={music1} type="audio/ogg" />
-          <source src="horse.mp3" type="audio/mpeg" />
-          Your browser does not support the audio element.
-        </audio>
-      </header>
+    <div className='container'>
+      <table>
+        <tr>
+          <th rowSpan={2}>
+            Vị trí
+          </th>
+          <th colSpan={3}>
+            Tương đương
+          </th>
+          <th colSpan={3}>
+            Khác biệt
+          </th>
+        </tr>
+
+        <tr>
+
+          <th rowSpan={1}>
+            Việt
+          </th>
+          <th rowSpan={1}>
+            Mường Bi
+          </th>
+          <th rowSpan={1}>
+            Mường Tân Sơn
+          </th>
+
+          <th rowSpan={1}>
+            Việt
+          </th>
+          <th rowSpan={1}>
+            Mường Bi
+          </th>
+          <th rowSpan={1}>
+            Mường Tân Sơn
+          </th>
+
+        </tr>
+        <tr>
+          <td style={{ fontWeight: 'bold', fontSize: '20px' }} rowSpan={24}>Âm đầu</td>
+          <td>-</td>
+          <td>/p/</td>
+          <td>/p/</td>
+          <td>-</td>
+          <td>/p/</td>
+          <td>/p/</td>
+
+
+        </tr>
+        <tr>
+
+          <td>/b/ b</td>
+          <td>/b/ b</td>
+          <td>/b/ b</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+        <tr>
+
+          <td>/m/ m</td>
+          <td>/m/ m</td>
+          <td>/m/ m</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+        <tr>
+
+          <td>/t/ t</td>
+          <td>/t/ t</td>
+          <td>/t/ t</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+        <tr>
+
+          <td>/d/ đ</td>
+          <td>/d/ đ</td>
+          <td>/d/ đ</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+        <tr>
+
+          <td>/th/</td>
+          <td>/th/</td>
+          <td>/th/</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+        <tr>
+
+          <td>// tr</td>
+          <td>-</td>
+          <td>// tr</td>
+          <td>// tr</td>
+          <td>-</td>
+          <td>// tr</td>
+
+
+        </tr>
+        <tr>
+
+          <td>/n/ n</td>
+          <td>/n/ n</td>
+          <td>/n/ n</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+        <tr>
+
+          <td>/s/ x</td>
+          <td>/s/ x</td>
+          <td>/s/ x</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+        <tr>
+
+          <td>// s</td>
+          <td>-</td>
+          <td>-</td>
+          <td>// s</td>
+          <td>-</td>
+          <td>-</td>
+
+
+        </tr>
+
+        <tr>
+
+          <td>/z/ d, gi</td>
+          <td>/z/ d</td>
+          <td>/z/ d</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr>
+
+          <td>/l/ l</td>
+          <td>/l/ l</td>
+          <td>/l/ l</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr>
+
+          <td>/c/ ch</td>
+          <td>/c/ ch</td>
+          <td>/c/ ch</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr>
+
+          <td>/ɲ/ nh</td>
+          <td>/ɲ/ nh</td>
+          <td>/ɲ/ nh</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+        <tr>
+
+          <td>/k/ k, c, q</td>
+          <td>/k/ k, c, q</td>
+          <td>/k/ k, c, q</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+        <tr>
+
+          <td>/ŋ/ ng, ngh</td>
+          <td>/ŋ/ ng, ng</td>
+          <td>/ŋ/ ng, ng</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+        <tr>
+
+          <td>/r/ r</td>
+          <td>/r/ r</td>
+          <td>/r/ r</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr>
+
+          <td>/ʔ/</td>
+          <td>/ʔ/</td>
+          <td>/ʔ/</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr>
+
+          <td>/h/ h</td>
+          <td>/h/ h</td>
+          <td>/h/ h</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+        <tr>
+
+          <td>/f/ ph</td>
+          <td>/f/ ph</td>
+          <td>/f/ ph</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+        <tr>
+
+          <td>/v/ v</td>
+          <td>-</td>
+          <td>/v/ v</td>
+          <td>/v/ v</td>
+          <td>-</td>
+          <td>/v/ v</td>
+
+
+
+        </tr>
+
+        <tr>
+
+          <td>// kh</td>
+          <td>// kh</td>
+          <td>// kh</td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+
+        <tr>
+
+          <td>-</td>
+          <td>/w/ w</td>
+          <td>/w/ w</td>
+          <td>-</td>
+          <td>/w/ w</td>
+          <td>-</td>
+
+        </tr>
+
+        <tr>
+          <td>-</td>
+          <td>tl (kl)</td>
+          <td>-</td>
+          <td>-</td>
+          <td>tl (kl)</td>
+          <td>-</td>
+        </tr>
+        {/* hang 2 */}
+        <tr>
+          <td style={{ fontWeight: 'bold', fontSize: '20px' }} >Bản nguyên âm</td>
+          <td>/w/ u, o</td>
+          <td>/w/ u, o</td>
+          <td>/w/ u, o</td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr>
+          <td style={{ fontWeight: 'bold', fontSize: '20px' }} rowSpan={16}>Âm chính</td>
+          <td>i /i/</td>
+          <td>i /i/</td>
+          <td>i /i/</td>
+          <td></td>
+          <td></td>
+        </tr>
+        <tr>
+          <td>/e/ ê</td>
+          <td>/e/ ê</td>
+          <td>/e/ ê</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+        </tr>
+
+        <tr>
+          <td>/ɛ/ e</td>
+          <td>/ɛ/ e</td>
+          <td>/ɛ/ e</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+        </tr>
+
+        <tr>
+          <td>/ɛ/ e</td>
+          <td>-</td>
+          <td>-</td>
+          <td>/ɛ/ e</td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr>
+          <td>/ɯ/ ư</td>
+          <td>/ɯ/ ư</td>
+          <td>/ɯ/ ư</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr>
+          <td>/ɤ/ ơ</td>
+          <td>/ɤ/ ơ</td>
+          <td>/ɤ/ ơ</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+        </tr>
+
+        <tr>
+          <td>/ɤ̆ / â</td>
+          <td>/ɤ̆ / â</td>
+          <td>/ɤ̆ / â</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+        </tr>
+        <tr>
+          <td>/a/ a</td>
+          <td>/a/ a</td>
+          <td>/a/ a</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+        </tr>
+        <tr>
+          <td>/ă/ ă</td>
+          <td>/ă/ ă</td>
+          <td>/ă/ ă</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+        </tr>
+        <tr>
+          <td>/u/ u</td>
+          <td>/u/ u</td>
+          <td>/u/ u</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+        </tr>
+        <tr>
+          <td>/o/ ô</td>
+          <td>/o/ ô</td>
+          <td>/o/ ô</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+        </tr>
+        <tr>
+          <td>/ɔ/ o</td>
+          <td>/ɔ/ o</td>
+          <td>/ɔ/ o</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+        </tr>
+        <tr>
+          <td>/ɔ/ o</td>
+          <td>-</td>
+          <td>-</td>
+          <td>/ɔ/ o</td>
+          <td></td>
+          <td></td>
+
+
+
+        </tr>
+
+        <tr>
+          <td>/ie/ iê</td>
+          <td>/ie/ iê</td>
+          <td>/ie/ iê</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr>
+          <td>/ɯɤ/ ươ</td>
+          <td>/ɯɤ/ ươ</td>
+          <td>/ɯɤ/ ươ</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr>
+          <td>/uo/ uô</td>
+          <td>/uo/ uô</td>
+          <td>/uo/ uô</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr >
+          <td style={{ fontWeight: 'bold', fontSize: '20px' }} rowSpan={11}>Âm cuối</td>
+          <td>/p/ p</td>
+          <td>/p/ p</td>
+          <td>/p/ p</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+        </tr>
+
+        <tr>
+          <td>/t/ t</td>
+          <td>/t/ t</td>
+          <td>/t/ t</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+        </tr>
+
+
+        <tr>
+          <td>/k/ c, ch</td>
+          <td>/k/ c</td>
+          <td>/k/ c</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+        </tr>
+
+
+        <tr>
+          <td>-</td>
+          <td>/c/ ch</td>
+          <td>/c/ ch</td>
+          <td>-</td>
+          <td>/c/ ch</td>
+          <td>/c/ ch</td>
+        </tr>
+
+        <tr>
+          <td>/m/ m</td>
+          <td>/m/ m</td>
+          <td>/m/ m</td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+
+        </tr>
+
+        <tr>
+          <td>/n/ n</td>
+          <td>/n/ n</td>
+          <td>/n/ n</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr>
+          <td>/ŋ/ nh, ng</td>
+          <td>/ŋ/ nh, ng</td>
+          <td>/ŋ/ nh, ng</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr>
+          <td>-</td>
+          <td>// nh</td>
+          <td>// nh</td>
+          <td>-</td>
+          <td>// nh</td>
+          <td>// nh</td>
+        </tr>
+
+        <tr>
+          <td>/w/ u, o</td>
+          <td>/w/ u, o</td>
+          <td>/w/ u, o</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+        </tr>
+
+        <tr>
+          <td>/j/ i, y</td>
+          <td>/j/ i, y</td>
+          <td>/j/ i, y</td>
+          <td></td>
+          <td></td>
+          <td></td>
+
+        </tr>
+        <tr>
+          <td>-</td>
+          <td>1</td>
+          <td>1</td>
+          <td>-</td>
+          <td>1</td>
+          <td>1</td>
+
+
+        </tr>
+
+        <tr >
+          <td style={{ fontWeight: 'bold', fontSize: '20px' }} rowSpan={7}>Thanh điệu </td>
+
+          <td style={{ fontWeight: 'bold', fontSize: '20px' }}>Việt </td>
+          <td style={{ fontWeight: 'bold', fontSize: '20px' }}>Mường Bi</td>
+          <td style={{ fontWeight: 'bold', fontSize: '20px' }}>Mường Bi Tân Sơn</td>
+          <td style={{ fontWeight: 'bold', fontSize: '20px' }}>Việt</td>
+          <td style={{ fontWeight: 'bold', fontSize: '20px' }}>Mường Bi</td>
+          <td style={{ fontWeight: 'bold', fontSize: '20px' }}>Mường Tân Sơn</td>
+
+
+        </tr>
+        <tr>
+          <td>Ngang, tần số
+            cao</td>
+          <td>Ngang, tần số
+            trung bình</td>
+          <td>đi ngang, bằng
+            phẳng, dài</td>
+          <td>A1 – Level {"<"}Ngang{">"}</td>
+          <td></td>
+          <td></td>
+
+
+
+        </tr>
+
+        <tr>
+          <td>Xuống, tần số
+            thấp</td>
+          <td>Xuống, tần số
+            thấp</td>
+          <td>đi xuống, thấp</td>
+          <td>A2 – Mid
+            falling
+            {"<"}Huyền{">"}</td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr>
+          <td>đi xuống, đi lên,
+            thấp, thanh quản
+            hóa</td>
+          <td>Lên, tần số cao</td>
+          <td>đi lên, cao, ngắn</td>
+          <td>C1 – Low
+            falling  {"<"}
+            Hỏi  {">"}</td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr>
+          <td>đi xuống, đi lên,
+            cao, thanh </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+          <td> </td>
+        </tr>
+
+        <tr>
+          <td>Lên cao</td>
+          <td>Xuống lên,
+            thanh quản
+            hoá ở đầu âm
+            tiết</td>
+          <td>đi ngang, ca0</td>
+          <td>B1 - Rising
+            {"<"}Sắc{">"}</td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+
+        <tr>
+          <td>Ngang (Xuống),
+            thấ</td>
+          <td>Ngang cao, kết
+            thúc tắc thanh
+            môn</td>
+          <td>Xuống thấp,
+            ngắn, kết thúc
+            tắc thanh môn</td>
+          <td>B2 – Low
+            glottalized
+            {"<"}Nặng {">"}</td>
+          <td></td>
+          <td></td>
+
+
+        </tr>
+      </table>
     </div>
   );
 }
